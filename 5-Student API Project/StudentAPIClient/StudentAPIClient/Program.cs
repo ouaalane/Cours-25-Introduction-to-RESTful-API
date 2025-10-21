b3f34b0dcb0c9919bf95daf2e2d0ce709c25193c
ouaalane <simoou3lane@gmail.com>
2025-10-21 18:30:37 +0200

    Update

---
+++ 5-Student API Project/StudentAPIClient/StudentAPIClient/Program.cs
@@ -0,0 +1,76 @@
+﻿
+
+
+using System.Net.Http.Json;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Grade { get; set; }
+
+
+}
+
+partial class Program
+{
+   
+
+    public static readonly HttpClient httpClient = new HttpClient();
+
+    public static async Task Main(string[] args)
+    {
+        
+        httpClient.BaseAddress = new Uri("http://localhost:5215/api/Students");
+
+        await GetAllStudents();
+
+
+
+
+        Console.ReadLine();
+
+
+
+
+
+    }
+
+
+    public static   async Task GetAllStudents()
+    {
+        
+        try
+        {
+
+            Console.WriteLine("\n------------------------------------------------------\n");
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\nFeatching Students ......\n");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var Students = await httpClient.GetFromJsonAsync<List<Student>>("");
+
+            if (Students!=null)
+            {
+                foreach (var student in Students)
+                {
+                    Console.WriteLine($"Student ID {student.Id} , Name : {student.Name} , Age : {student.Age}");
+
+                }
+            }
+            
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine( "An Error Accured :(" + ex.Message);
+        }
+       
+       
+        
+
+
+    }
+}
---
+++ 5-Student API Project/StudentAPIServer/MyFirstRestAPI_Porject/Controllers/StudentAPIController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+using StudentApi.Models;
+using StudentApi.DataSimulation;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace StudentApi.Controllers 
+{
+    [ApiController] // Marks the class as a Web API controller with enhanced features.
+   // [Route("[controller]")] // Sets the route for this controller to "students", based on the controller name.
+    [Route("api/Students")]
+
+    public class StudentsController : ControllerBase // Declare the controller class inheriting from ControllerBase.
+    {
+        
+        [HttpGet("All",Name ="GetAllStudents")] // Marks this method to respond to HTTP GET requests.
+        public ActionResult<IEnumerable<Student>> GetAllStudents() // Define a method to get all students.
+        {
+            return Ok(StudentDataSimulation.StudentsList); // Returns the list of students.
+        }
+
+
+        [HttpGet("Passed",Name ="GetPassedStudensts")]
+        public ActionResult<IEnumerable<Student>> GetPassedStudents()
+        {
+            
+
+            if (!StudentDataSimulation.StudentsList.Any())
+                return NoContent(); 
+
+            return Ok(StudentDataSimulation.StudentsList.Where(st=>st.Grade>=60)); 
+        }
+    }
+}
